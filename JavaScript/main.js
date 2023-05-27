cc4b4b1a79ad4e81f31e00ef0a7c9782e993ce91
Alex Rodriguez <alxrg7@gmail.com>
2023-05-26 20:43:59 -0500

    Tarea algoritmos

---
+++ JavaScript/main.js
@@ -0,0 +1,27 @@
+// Función para convertir litros a centilitros
+function litrosACentilitros(litros) {
+  return litros * 100;
+}
+
+// Función para convertir litros a decilitros
+function litrosADecilitros(litros) {
+  return litros * 10;
+}
+
+// Función para convertir litros a galones
+function litrosAGalones(litros) {
+  return litros * 0.264172;
+}
+
+// Función para convertir litros a onzas
+function litrosAOnzas(litros) {
+  return litros * 33.814;
+}
+
+// Ejemplo de uso
+var litros = 2.5;
+console.log(litros + " litros son equivalentes a:");
+console.log(litrosACentilitros(litros) + " centilitros");
+console.log(litrosADecilitros(litros) + " decilitros");
+console.log(litrosAGalones(litros) + " galones");
+console.log(litrosAOnzas(litros) + " onzas");
